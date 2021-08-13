359797ab7cddd745ba73ba01eba571461e671ddf
Muhammad Sayuti <sayutizxc@gmail.com>
2021-08-13 19:36:49 +0800

    hapus api yang tidak digunakan

--- lib/constant/api.dart
+++ lib/constant/api.dart
@@ -5,8 +5,5 @@ const String apiCovidUpdate =
 /// Api covid-19 setiap provinsi
 const String apiCovidProv = 'https://data.covid19.go.id/public/api/prov.json';
 
-/// Api rumah sakit rujunak covid-19
-const String apiCovidRs = 'https://dekontaminasi.com/api/id/covid19/hospitals';
-
 /// Api artikel kesehatan
 const String apiArtikelKesehatan = 'https://www.halodoc.com/api/cms/articles';
